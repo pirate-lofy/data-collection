5f81760938031ab67ef2dd77a5a77b8310f175d9
Nour Helal <nour.helal@fotopiatech>
2023-09-12 17:26:46 +0300

    working camera class with sync data retrieval

--- cameras/camera.py
+++ cameras/camera.py
@@ -4,7 +4,7 @@ from common import *
 import numpy as np
 
 try:
-    sys.path.append("carla-0.9.5-py3.5-linux-x86_64.egg")
+    sys.path.append("carla-0.9.13-py3.7-linux-x86_64.egg")
 except IndexError:
     printc('cant append carla #egg',MsgType.ERROR)
 
@@ -13,33 +13,42 @@ from carla import ColorConverter as cc
 
 class Camera:
     
-    def __init__(self,name,dic,vehicle):
+    def __init__(self,name:str,settings:dict,vehicle,world):
+        '''
+            @input:
+                name: name of the camera to be referenced with
+                settings: settings of the camera comes from conf file
+                vehicle: spawned vehicle 
+                world: carla world
+        '''
         self.name=name
         self.vehicle=vehicle
-        self.dic=AttrDict(dic)
+        self.settings=AttrDict(settings)
+        self.world=world
+        self.blueprint=self.world.get_blueprint_library()
 
         self.init_attr()
-        self._make_cam(dic)
+        self._make_cam()
 
     def init_attr(self):
         self.data={}
         self.actors=[]
 
-    def _make_cam(self,dic):
-        image_x=self.dic.image_x
-        image_y=self.dic.image_y
-        fov=self.dic.fov
+    def _make_cam(self):
+        image_x=self.settings.image_x
+        image_y=self.settings.image_y
+        fov=self.settings.fov
 
-        loc=self.dic.location
+        loc=self.settings.location
         x,y,z=[*loc.values()]
-        ori=self.dic.orientation
+        ori=self.settings.orientation
         pitch,yaw,roll=[*ori.values()]
 
         
         
         transform = carla.Transform(carla.Location(x=x, y=y, z=z),
                                     carla.Rotation(pitch=pitch, yaw=yaw, roll=roll))
-        # rgb cam
+        # rgb sensor
         rgb = self.blueprint.find('sensor.camera.rgb')
         rgb.set_attribute('image_size_x', f"{image_x}")
         rgb.set_attribute('image_size_y', f"{image_y}")
@@ -48,7 +57,7 @@ class Camera:
         rgb.listen(lambda data: self._process_rgb(data))
         self.actors.append(rgb)
         
-        # segmentation cam
+        # segmentation sensor
         seg = self.blueprint.find('sensor.camera.semantic_segmentation')
         seg.set_attribute('image_size_x', f"{image_x}")
         seg.set_attribute('image_size_y', f"{image_y}")
@@ -57,7 +66,7 @@ class Camera:
         seg.listen(lambda data: self._process_seg(data))
         self.actors.append(seg)
         
-        # depth cam
+        # depth sensor
         dep=self.blueprint.find('sensor.camera.depth')
         dep.set_attribute('image_size_x', f"{image_x}")
         dep.set_attribute('image_size_y', f"{image_y}")
